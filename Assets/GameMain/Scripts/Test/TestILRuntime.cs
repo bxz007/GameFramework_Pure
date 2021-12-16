8526b04a40069a4afac4ac5eaf741b2bf6e12d58
bxz007 <624857280@qq.com>
2021-12-16 21:37:24 +0800

    添加ILRuntime  添加简单测试

---
+++ Assets/GameMain/Scripts/Test/TestILRuntime.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using GameMain;
+public class TestILRuntime : MonoBehaviour
+{
+    ILRuntime.Runtime.Enviorment.AppDomain appdomain;
+    void Start()
+    {
+        StartCoroutine(LoadILRuntime());
+    }
+
+    IEnumerator LoadILRuntime()
+    {
+        appdomain = new ILRuntime.Runtime.Enviorment.AppDomain();
+#if UNITY_ANDROID
+    WWW www = new WWW(Application.streamingAssetsPath + "/Hotfix.dll");
+#else
+        WWW www = new WWW("file:///" + Application.streamingAssetsPath + "/hotfix.dll");
+#endif
+        while (!www.isDone)
+            yield return null;
+        if (!string.IsNullOrEmpty(www.error))
+            Debug.LogError(www.error);
+        byte[] dll = www.bytes;
+        www.Dispose();
+#if UNITY_ANDROID
+    www = new WWW(Application.streamingAssetsPath + "/Hotfix.pdb");
+#else
+        www = new WWW("file:///" + Application.streamingAssetsPath + "/hotfix.pdb");
+#endif
+        while (!www.isDone)
+            yield return null;
+        if (!string.IsNullOrEmpty(www.error))
+            Debug.LogError(www.error);
+        byte[] pdb = www.bytes;
+        System.IO.MemoryStream fs = new MemoryStream(dll);
+        System.IO.MemoryStream p = new MemoryStream(pdb);
+        appdomain.LoadAssembly(fs, p, new ILRuntime.Mono.Cecil.Pdb.PdbReaderProvider());
+
+        OnILRuntimeInitialized();
+    }
+
+    void OnILRuntimeInitialized()
+    {
+        appdomain.Invoke("Hotfix.TestHotFixMain", "Initialize", null, null);
+    }
+}
